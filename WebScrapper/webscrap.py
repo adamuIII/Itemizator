f23954392ab0165ce2e847b17c91e5389e3d015f
adamuIII <adamrzepka32167@gmail.com>
2023-05-16 19:30:15 +0200

    scraping from all guild members

--- WebScrapper/webscrap.py
+++ WebScrapper/webscrap.py
@@ -25,44 +25,41 @@ acceptCookiesButton.click()
 characterButton = driver.find_element(By.XPATH, '/html/body/div[4]/div[2]/div/div[2]/div[1]/ul/li[7]/a')
 characterButton.click()
 
-
 # WebDriverWait(driver,timeout=30).until(lambda d: d.find_element(By.XPATH,'/html/body/div[2]/div/a'))
 wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div/a')))
 acceptCookiesButton2 = driver.find_element(By.XPATH, '/html/body/div[2]/div/a')
 acceptCookiesButton2.click()
 
-# WebDriverWait(driver,timeout=30).until(lambda d: d.find_element(By.XPATH,'/html/body/div[4]/div[4]/div[2]/div/div/div[3]/div[6]/div[1]/a[1]'))
-wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="DataTables_Table_3"]/tbody/tr[1]/td[1]/a')))
-driver.find_element(By.XPATH, '//*[@id="DataTables_Table_3"]/tbody/tr[1]/td[1]/a').click()
-
-
-wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="boss-table-1017"]/tbody/tr[1]/td[1]/div/div[1]/a')))
-driver.find_element(By.XPATH, '//*[@id="boss-table-1017"]/tbody/tr[1]/td[1]/div/div[1]/a').click()
-
-wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="boss-table"]/tbody/tr[1]/td[3]/a')))
-dps = driver.find_element(By.XPATH, '//*[@id="boss-table"]/tbody/tr[1]/td[3]/a')
-timesKilled = driver.find_element(By.XPATH, '//*[@id="top-box"]/div[2]/table/tbody/tr[2]/td[2]').text
-driver.find_element(By.XPATH, '//*[@id="boss-table"]/thead/tr/th[6]/div').click()
-driver.find_element(By.XPATH, '//*[@id="boss-table"]/thead/tr/th[6]/div').click()
-temp = int(timesKilled)
-dpsList = {}
-
-for x in range(temp-1):
-    # dpsStr = driver.find_element(By.XPATH, '//*[@id="boss-table"]/tbody/tr['+str(x+2)+']/td[3]/a')
-    # dpsList.append(dpsStr.text)
-    # //*[@id="boss-table"]/tbody/tr[1]/td[4]
-    dpsList.append(driver.find_element(By.XPATH, '//*[@id="boss-table"]/tbody/tr['+str(x+2)+']/td[3]/a').text)
-
-print(dpsList)
-plt.bar(range(temp),dpsList)
-plt.show()
 
 
 
+def saveData(playerIndex):
+    # WebDriverWait(driver,timeout=30).until(lambda d: d.find_element(By.XPATH,'/html/body/div[4]/div[4]/div[2]/div/div/div[3]/div[6]/div[1]/a[1]'))
+    wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="DataTables_Table_3"]/tbody/tr['+str(playerIndex)+']/td[1]/a')))
+    driver.find_element(By.XPATH, '//*[@id="DataTables_Table_3"]/tbody/tr['+str(playerIndex)+']/td[1]/a').click()
 
 
+    wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="boss-table-1017"]/tbody/tr[1]/td[1]/div/div[1]/a')))
+    driver.find_element(By.XPATH, '//*[@id="boss-table-1017"]/tbody/tr[1]/td[1]/div/div[1]/a').click()
+
+    wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="boss-table"]/tbody/tr[1]/td[3]/a')))
+    dps = driver.find_element(By.XPATH, '//*[@id="boss-table"]/tbody/tr[1]/td[3]/a')
+    timesKilled = driver.find_element(By.XPATH, '//*[@id="top-box"]/div[2]/table/tbody/tr[2]/td[2]').text
+    temp = int(timesKilled)
+    dpsList = {}
+
+    for x in range(temp-1):
+        # dpsList.append(driver.find_element(By.XPATH, '//*[@id="boss-table"]/tbody/tr['+str(x+2)+']/td[3]/a').text)
+        dpsList.setdefault(driver.find_element(By.XPATH, '//*[@id="boss-table"]/tbody/tr['+str(x+1)+']/td[3]/a').text,[]).append(driver.find_element(By.XPATH, '//*[@id="boss-table"]/tbody/tr['+str(x+1)+']/td[4]').text)
+
+    print(dpsList)
+    # driver.back()
+    # driver.back()
+    driver.execute_script("window.history.go(-2)")
+    wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="DataTables_Table_3"]/tbody/tr['+str(playerIndex+1)+']/td[1]/a')))
 
 
+for x in range(20):
+    saveData(x+1)
 while(True):
     pass
-
